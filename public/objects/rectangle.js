1f03f18cfd1b666d1375f1b166e3b16628a05805
Akshay Diwan <akshaydiwan06@gmail.com>
2025-03-03 19:08:53 +0530

    completed version 1

--- public/objects/rectangle.js
+++ public/objects/rectangle.js
@@ -1,5 +1,5 @@
 import canvas from './Dom.js'
-function createRectangle (rectangle){
+export function createRectangle(rectangle){
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
     ctx.strokeStyle = rectangle.color || '#000000';
@@ -59,4 +59,74 @@ function createRectangle (rectangle){
       ctx.lineWidth = 1;
     }
   }
-  export default createRectangle;   
+
+  export function createLine(line){
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+      ctx.strokeStyle = line.color || '#000000';
+      ctx.lineWidth = line.strokeWidth || 3;
+      ctx.globalAlpha = line.opacity || 1;
+  
+      // Draw line
+      ctx.beginPath();
+      ctx.moveTo(line.x,line.y);
+      ctx.lineTo(line.endX,line.endY);
+      ctx.stroke();
+      ctx.strokeStyle ='#000000';
+      ctx.lineWidth = 1;
+      ctx.fillStyle = '#0000FF00';
+  }
+  }
+  export function createArbitary(shape, multX = 1, multY = 1,moveX = 0, moveY = 0){
+    if(!shape.points){
+      return;
+    }
+    shape.points = JSON.parse(shape.points).map((point) => ({
+      x: point.x * multX + moveX,
+      y: point.y * multY + moveY
+    }));
+  
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+      ctx.strokeStyle = shape.color || '#000000';
+      ctx.lineWidth = shape.strokeWidth || 5;
+      ctx.globalAlpha = shape.opacity || 1;
+      //handwritten
+      ctx.beginPath();
+      ctx.moveTo(shape.points[0].x,shape.points[0].y);
+      for(let j in shape.points){
+        ctx.lineTo(shape.points[j].x , shape.points[j].y);
+      }
+      ctx.stroke();
+      ctx.strokeStyle ='#000000';
+      ctx.lineWidth = 1;
+      shape.points = JSON.stringify(shape.points);
+  }
+  }
+  export function createTextField(shape){
+    if(canvas.getContext){
+      const ctx = canvas.getContext("2d");
+      console.log(shape.text);
+      console.log(shape.x);
+      console.log(shape.y);
+    
+
+      // ctx.fillText(shape.text, shape.x, shape.y);
+      ctx.font = "30px Arial"; // Set the font
+      ctx.fillStyle = "black";
+      ctx.textBaseline = "hanging";
+      ctx.fillText(shape.text, shape.x, shape.y);
+      const text = ctx.measureText(shape.text);
+      shape.height = 16;
+      shape.width = text.width;
+      console.log(shape.height);
+      console.log(shape.width);
+    }
+  }
+  export default function clearCanvas(){
+    if(canvas.getContext){
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  };
+  
